c18dca585ecc7fe6b6ffb4d3d70db6f2eaa5a0fb
Joe Wales <joewales13@gmail.com>
2017-12-22 09:38:30 -0600

    fixed picks controller

--- server/public/scripts/controllers/picks.controller.js
+++ server/public/scripts/controllers/picks.controller.js
@@ -1,5 +1,5 @@
-myApp.controller('PicksController', function(MatchupService) {
+myApp.controller('PicksController', ['UserService', 'MatchupService', function(UserService, MatchupService) {
     console.log('PicksController created');
     var vm = this;
     
-  });
+  }]);
